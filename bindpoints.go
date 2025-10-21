c060d55c8ee452b527e3d8d639fcbd05c550886f
dovholuknf <46322585+dovholuknf@users.noreply.github.com>
2025-10-21 09:11:15 -0400

    add comments and file header

--- bindpoints.go
+++ bindpoints.go
@@ -1,3 +1,19 @@
+/*
+	Copyright NetFoundry Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+	https://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
 package xweb
 
 import (
@@ -8,17 +24,21 @@ import (
 	"github.com/openziti/identity"
 )
 
+// BindPointListenerFactoryRegistry is used to add BindPointListenerFactory instances that are used to return
+// new BindPoint instances
 var BindPointListenerFactoryRegistry []BindPointListenerFactory
 
+// BindPointListenerFactory is an interface that will generate new BindPoint instances based on configuration
 type BindPointListenerFactory interface {
 	New(map[interface{}]interface{}) (BindPoint, error)
 }
 
+// The BindPoint interface is used to provide necessary information to xweb. Primarily, it is used to provide
+// listeners to the http server xweb controls.
 type BindPoint interface {
 	Listener(serverName string, tlsConfig *gotls.Config) (net.Listener, error) // a listener to be used with the http server
 	BeforeHandler(next http.Handler) http.Handler                              // called before xweb handlers execute
 	AfterHandler(prev http.Handler) http.Handler                               // called after xweb handlers complete
 	Validate(identity.Identity) []error                                        // validates the BindPoint
 	ServerAddress() string                                                     // the address the server
-	Configure(config []interface{}) error                                      // configures the BindPoint using the provided map
 }
